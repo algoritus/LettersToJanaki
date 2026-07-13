297f8320968e6db94fa2a7b5f5630a26c50b5985
Algoritus <hello.algoritus@gmail.com>
2026-07-13 17:31:11 +0530

    Secure Supabase keys

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bceufurrruwukbvwtzsj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjZXVmdXJycnV3dWtidnd0enNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MjgyODIsImV4cCI6MjA5OTUwNDI4Mn0.FDvn1mFlv5ii2ON40aXaH4iK2d16ov4Jol2C4LY9-oU"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
-
